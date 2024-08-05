0c2b6ee061b5d929b2672f4c74fd079f33443be6
emiliano <emilian.chiper.developer@gmail.com>
2024-08-05 10:55:42 +0300

    Implement validation for name inputs

--- src/js/script.js
+++ src/js/script.js
@@ -8,18 +8,50 @@ window.addEventListener('DOMContentLoaded', function () {
       errorLastName,
       errorEmail,
       errorPassword,
-      errrorConfirmPassword,
+      errorConfirmPassword,
     ] = document.getElementsByTagName('small');
 
-    console.log(
-      errorFirstName,
-      errorLastName,
-      errorEmail,
-      errorPassword,
-      errrorConfirmPassword
-    );
+    // Set initial state of the error elements
+    const state = function () {
+      [...document.getElementsByTagName('small')].forEach(small => {
+        small.style.opacity = '0';
+        small.innerText = '_';
+        small.classList.remove('valid', 'invalid');
+      });
+    };
 
     // Check max length of first and last name
+    const checkMaxLength = function (input, handler) {
+      if (input.value.length === 0) {
+        // Clear the error message if the input is empty
+        handler.style.opacity = '0';
+        handler.innerText = '-';
+        handler.classList.remove('valid', 'invalid');
+      } else if (input.value.length <= 35) {
+        // Valid input
+        handler.style.opacity = '1';
+        handler.innerText = 'Valid name';
+        handler.classList.add('valid');
+        handler.classList.remove('invalid');
+      } else {
+        // Invalid input
+        handler.style.opacity = '1';
+        handler.innerText = 'Maximum number of characters: 35';
+        handler.classList.add('invalid');
+        handler.classList.remove('valid');
+      }
+    };
+
+    // Invoke functions
+    state();
+
+    // Assign callbacks to event listeners
+    firstName.addEventListener('input', () =>
+      checkMaxLength(firstName, errorFirstName)
+    );
+    lastName.addEventListener('input', () =>
+      checkMaxLength(lastName, errorLastName)
+    );
   };
 
   main();
