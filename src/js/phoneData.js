f330d1fffadeb255956962e13dc5bab6de0cf504
emiliano <emilian.chiper.developer@gmail.com>
2024-08-07 16:54:50 +0300

    Comment content of phoneData.js

--- src/js/phoneData.js
+++ src/js/phoneData.js
@@ -1,3 +1,15 @@
+/**
+ * Fetches country data from a JSON file.
+ *
+ * @async
+ * @function fetchCountryData
+ * @returns {Promise<Object>} A promise that resolves to the JSON object containing country data.
+ * @throws {Error} Will throw an error if the fetch request fails.
+ * @example
+ * fetchCountryData()
+ *   .then(data => console.log(data))
+ *   .catch(error => console.error(error));
+ */
 export const fetchCountryData = async () => {
   try {
     const response = await fetch('src/data/countries.json');
